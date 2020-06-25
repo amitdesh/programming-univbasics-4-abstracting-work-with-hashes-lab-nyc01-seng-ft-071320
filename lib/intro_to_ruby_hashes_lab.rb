3237563dffb75ad30c4bd127104d19e502f87c20
A D <amitdesh2014@gmail.com>
2020-06-25 01:39:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,11 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  if hash[:key] == True
-    return hash[:key]
+  if hash[key]
+    return hash[key]
   else
     return nil
+  end
 end
 
 def update_counting_hash(hash, key)
